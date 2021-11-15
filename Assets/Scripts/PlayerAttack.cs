3eb33e45c1cf09a4e63098f1baac193f2539c5a1
Yassine Salhi <yassinesalhi135@gmail.com>
2021-11-15 12:24:55 +0000

    added walls, ceiling and floot, attack is done, working on camera movement

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private GameObject[] fireballs;
+
+
     private Animator anim;
     private PlayerController playerMovement;
     private float coolDownTimer = Mathf.Infinity;
@@ -29,6 +33,21 @@ public class PlayerAttack : MonoBehaviour
     {
         anim.SetTrigger("Attack");
         coolDownTimer = 0;
+
+        fireballs[FindFireball()].transform.position = attackPoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
     }
 
 }
