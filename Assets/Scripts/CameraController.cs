2335efa2e3626a149177799b05c725b0c95e2ec2
Yassine Salhi <yassinesalhi135@gmail.com>
2021-11-15 12:59:05 +0000

    added camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Room camera
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    //Follow player
+    public float smoothing; //dampening 
+    Vector3 offset;
+    float lowY;
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+
     void Start()
     {
-        
+        offset = transform.position - player.position;
+
+        lowY = transform.position.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        //Room camera
+        // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //Follow player
+        FollowPlayer();
+        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    private int getYAxis()
+    {
+
+        if(player.position.y < 1)
+        {
+            return 0;
+        }
+
+        return (int) player.position.y;
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 targetCamPos = player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        if (transform.position.y < lowY)
+        {
+            transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
+        }
     }
 }
